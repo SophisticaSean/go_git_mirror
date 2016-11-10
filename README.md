7a4a30522a09370f4c32e7555d895742212f2bdd
Sean Lewis <sensaisean@gmail.com>
2016-11-10 00:50:18 -0700

    add readme, it actually works.jpg

---
+++ README.md
@@ -0,0 +1,2 @@
+A quck go program that'll read from a config.json file and watch the repos you specify, and populate those changes out to the mirrors you specify.
+See the config.json file for an example
--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
 )
 
 // Repos is a struct representing a JSON configuration repo obj
 type Repos struct {
 	Name    string `json:"name"`
+	Path    string `json:"path"`
 	Source  Repo   `json:"source"`
 	Mirrors []Repo `json:"mirrors"`
 }
@@ -21,22 +27,84 @@ type Repo struct {
 
 // Configuration is the root struct our configuration file
 type Configuration struct {
-	Repos []Repos `json:"repos"`
+	Repos    []Repos `json:"repos"`
+	Interval int     `json:"interval"`
+	HomePath string  `json:"pathToThisFile"`
 }
 
-func main() {
+var configuration Configuration
+
+func blahinit() {
 	file, err := ioutil.ReadFile("./conf.json")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(string(file))
-	var configuration Configuration
 	err = json.Unmarshal(file, &configuration)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(configuration)
-	for i := range configuration.Repos {
-		fmt.Println(configuration.Repos[i])
+}
+
+func updateConfiguration(homePath string) {
+	errOne := os.Chdir(homePath)
+	if errOne != nil {
+		panic(errOne)
+	}
+	file, err := ioutil.ReadFile("./conf.json")
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal(file, &configuration)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func commandWrapper(firstArg string, commands []string) (out string, newErr string) {
+	cmd := exec.Command(firstArg, commands...)
+	var buf bytes.Buffer
+	var stdErr bytes.Buffer
+	cmd.Stdout = &buf
+	cmd.Stderr = &stdErr
+
+	err := cmd.Start()
+	if err != nil {
+		panic(err)
+	}
+
+	cmd.Wait()
+	out = buf.String()
+	newErr = stdErr.String()
+	return out, newErr
+}
+
+func main() {
+	fmt.Println("running")
+	blahinit()
+	for {
+		updateConfiguration(configuration.HomePath)
+		for i := range configuration.Repos {
+			repo := configuration.Repos[i]
+			mirrors := repo.Mirrors
+			err := os.Chdir(repo.Path)
+			if err != nil {
+				panic(err)
+			}
+			output, _ := commandWrapper("git", []string{"pull", repo.Source.Name, "master"})
+			if !strings.Contains(output, "up-to-date") {
+				fmt.Println("Picked up changes from " + repo.Source.Name)
+				fmt.Println(output)
+			}
+			for i := range mirrors {
+				mirror := mirrors[i]
+				output, err := commandWrapper("git", []string{"push", mirror.Name, "master"})
+				if !strings.Contains(err, "up-to-date") {
+					fmt.Println("Pushing changes out to " + mirror.Name)
+					fmt.Println(output, err)
+				}
+			}
+			//fmt.Println(configuration.Repos[i])
+		}
+		time.Sleep(time.Duration(configuration.Interval) * time.Second)
 	}
 }
